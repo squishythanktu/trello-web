deb949bd151612732ca1b95ceecb0c61ee26bcf0
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2024-01-24 14:45:46 +0700

    feat: drag & drop columns

--- src/pages/Board/BoardContent/BoardContent.tsx
+++ src/pages/Board/BoardContent/BoardContent.tsx
@@ -1,27 +1,56 @@
 import Box from '@mui/material/Box'
 import theme from 'src/theme'
 import ListColumns from './ListColumns'
-import { Board } from 'src/apis/mock-data'
+import { Board, Column } from 'src/apis/mock-data'
 import sortBy from 'lodash/sortBy'
 import indexOf from 'lodash/indexOf'
+import { DndContext, DragEndEvent, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
 interface Props {
   board: Board
 }
 
 export default function BoardContent({ board }: Props) {
+  // Require mouse to move at least 10px then trigger event, avoid clicking trigger event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
   const customTheme = theme as any
-  const sortedColumns = sortBy(board.columns, (column) => indexOf(board.columnOrderIds, column._id))
+  const [orderedColumns, setOrderedColumns] = useState<Column[]>([])
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  useEffect(() => {
+    const sortedColumns = sortBy(board.columns, (column) => indexOf(board.columnOrderIds, column._id))
+    setOrderedColumns(sortedColumns)
+  }, [board])
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+
+    if (!over) return
+
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex((column) => column._id === active.id)
+      const newIndex = orderedColumns.findIndex((column) => column._id === over.id)
+
+      // use arrayMove of dnd-kit to sort original columns array & set state
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
 
   return (
-    <Box
-      className='w-full py-[10px]'
-      sx={{
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-        height: customTheme.trello.boardContentHeight
-      }}
-    >
-      <ListColumns columns={sortedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        className='w-full py-[10px]'
+        sx={{
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+          height: customTheme.trello.boardContentHeight
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
--- src/pages/Board/BoardContent/ListColumns/ListColumns.tsx
+++ src/pages/Board/BoardContent/ListColumns/ListColumns.tsx
@@ -3,6 +3,7 @@ import Column from './Column'
 import Button from '@mui/material/Button'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
 import { Column as ColumnType } from 'src/apis/mock-data'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 interface Props {
   columns: ColumnType[]
@@ -10,23 +11,25 @@ interface Props {
 
 export default function ListColumns({ columns }: Props) {
   return (
-    <Box
-      className='flex h-full w-full overflow-x-auto overflow-y-hidden bg-inherit'
-      sx={{
-        '&::-webkit-scrollbar-track': { m: 2 }
-      }}
-    >
-      {columns?.map((column, index) => <Column key={index} column={column} />)}
+    <SortableContext items={columns?.map((column) => column._id)} strategy={horizontalListSortingStrategy}>
       <Box
-        className='mx-4 h-fit min-w-[200px] max-w-[500px] rounded-md'
+        className='flex h-full w-full overflow-x-auto overflow-y-hidden bg-inherit'
         sx={{
-          bgcolor: '#ffffff3d'
+          '&::-webkit-scrollbar-track': { m: 2 }
         }}
       >
-        <Button className='w-full justify-start py-2 pl-5 text-white' startIcon={<NoteAddIcon />}>
-          Add new column
-        </Button>
+        {columns?.map((column, index) => <Column key={index} column={column} />)}
+        <Box
+          className='mx-4 h-fit min-w-[200px] max-w-[500px] rounded-md'
+          sx={{
+            bgcolor: '#ffffff3d'
+          }}
+        >
+          <Button className='w-full justify-start py-2 pl-5 text-white' startIcon={<NoteAddIcon />}>
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
